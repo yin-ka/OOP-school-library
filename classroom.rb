4bce63aebd3b0dc67aca4d1c45e49933612b6a2a
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-06 16:52:19 +0100

    Create a classroom has many students

--- classroom.rb
+++ classroom.rb
@@ -1,8 +1,16 @@
-class Classroom
-    attr_accessor :label
-  
-    def initialize(label)
-      @label = label
-    end
+require_relative './student'
+
+class Classroom < Student
+  attr_reader :student
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @students = []
   end
-  
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end

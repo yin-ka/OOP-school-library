e5f4190b2432c626f525af324a162846ff30cb51
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-12 08:15:44 +0100

    Refactor user_input.rb file

--- user_input.rb
+++ user_input.rb
@@ -3,23 +3,29 @@ require './create_person'
 require './create_book'
 require './create_rental'
 class Input
-    # app = App.new
-    def user_input(app,num)
+
+    def user_input(app, num)
         case num
-        when 1
-          app.book_list
-        when 2
-          app.people_list
-        when 3
-          CreatePerson.new(app.people).create_person
-        when 4
-          CreateBook.new(app.books).create_book
-        when 5
-          CreateRental.new(app.rentals).create_rental(app)
-        when 6
-          app.rental_list
-        when !(1...7)
-            p 'Invalid option, please choose again'
-        end
+        when 1 then app.book_list
+        when 2 then app.people_list
+        when 3 then create_person(app)
+        when 4 then create_book(app)
+        when 5 then create_rental(app)
+        when 6 then app.rental_list
+        else
+          p 'Invalid option, please choose again'
     end
+end
+
+def create_person(app)
+  CreatePerson.new(app.people).create_person
+end
+
+def create_book(app)
+  CreateBook.new(app.books).create_book
+end
+
+def create_rental(app)
+  CreateRental.new(app.rentals).create_rental(app)
+end
 end

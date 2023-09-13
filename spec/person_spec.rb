6081630e256ce3901134ae83a991bd5625b76b69
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-13 19:50:14 +0100

    Fix linters errors

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe Person do
-    before :each do
-        @person = Person.new(15, "Joe")
-    end
-    it "Should return a person object" do
-      expect(@person).to be_an_instance_of Person
-    end
+  before :each do
+    @person = Person.new(15, 'Joe')
+  end
+  it 'Should return a person object' do
+    expect(@person).to be_an_instance_of Person
+  end
 
-describe "#age" do
-    it "Should return Person age" do
+  describe '#age' do
+    it 'Should return Person age' do
       expect(@person.age).to eql 15
     end
-end
+  end
 
-describe "#name" do
-    it "Should return Person name" do
-        expect(@person.name).to eql "Joe"
+  describe '#name' do
+    it 'Should return Person name' do
+      expect(@person.name).to eql 'Joe'
     end
-end
+  end
 end

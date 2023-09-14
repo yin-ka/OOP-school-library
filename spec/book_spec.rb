957366da320d0ebf5e8de4319217b1f6718efeb7
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-14 17:54:09 +0100

    make changes

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -20,24 +20,26 @@ describe Book do
       expect(@book.author).to eql 'Author'
     end
   end
+end
 
-  describe '#add_rental' do
-    it 'should add a rental to the book' do
-      rental = Rental.new('John Doe', '2023-09-14', '2023-09-21')
-      @book.add_rental(rental)
-      expect(@book.rentals).to include(rental)
+describe Book do
+  before :each do
+    @book = Book.new('Title', 'Author')
+  end
+
+  it 'should return a Book object' do
+    expect(@book).to be_an_instance_of Book
+  end
+
+  describe '#title' do
+    it 'should return a book title' do
+      expect(@book.title).to eql 'Title'
     end
   end
 
-  describe '#to_json' do
-    it 'should return book data in JSON format' do
-      json_data = {
-        title: 'Title',
-        author: 'Author',
-        rentals: []
-      }.to_json
-
-      expect(@book.to_json).to eql json_data
+  describe '#author' do
+    it 'should return a book author' do
+      expect(@book.author).to eql 'Author'
     end
   end
 end

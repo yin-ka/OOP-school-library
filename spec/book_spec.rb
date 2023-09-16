52abc3485c756cf509d526adf7efc8f785ae0a4b
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-09-16 18:00:52 +0100

    Make changes proposed by reviewers

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,19 +11,15 @@ describe Book do
     expect(@book).to be_an_instance_of Book
   end
 
-  describe '#title' do
-    it 'should return a book title' do
-      expect(@book.title).to eql 'Title'
-    end
+  it 'should return a book title' do
+    expect(@book.title).to eql 'Title'
   end
 
-  describe '#author' do
-    it 'should return a book author' do
-      expect(@book.author).to eql 'Author'
-    end
+  it 'should return a book author' do
+    expect(@book.author).to eql 'Author'
   end
 
-  describe '#add_rental' do
+  context 'when adding a rental' do
     it 'should create a new rental and add it to the rentals array' do
       @book.add_rental(@person, @date)
       expect(@book.rentals.first).to be_an_instance_of Rental
@@ -32,12 +28,10 @@ describe Book do
     end
   end
 
-  describe '#to_json' do
-    it 'should return a JSON representation of the book' do
-      json = @book.to_json
-      expect(json).to be_a String
-      expect(json).to include('Title')
-      expect(json).to include('Author')
-    end
+  it 'should return a JSON representation of the book' do
+    json = @book.to_json
+    expect(json).to be_a String
+    expect(json).to include('Title')
+    expect(json).to include('Author')
   end
 end
